8e2ad45167d02620d2dfa86b85c4c1421c68ba3b
Katie Hampson <katiehampson@gmail.com>
2023-11-28 15:35:07 +0000

    Fixed code errors
    Cleaned up bugs in explore_case_data.R

--- explore_case_data.R
+++ explore_case_data.R
@@ -1,9 +1,17 @@
 
+# Clear workspace
 rm(list=ls())
 
+# Read in case data
 case_data <- read.csv("case_data.csv")
 
-par(mfrow=c(3,2), mar=c(5,5,1,1))
+# Identify categorical variables
+case_data[,c("urban")] <- as.factor(case_data[,c("urban")]) 
+case_data[,c("coastal")] <- as.factor(case_data[,c("coastal")]) 
+
+# Plot cases against possible explanatory variables and save as pdf
+pdf("exploratory_plots.pdf", width=7, height=6)
+par(mfrow=c(2,2), mar=c(5,5,1,1))
 
 # Cases against dogs
 plot(case_data$cases~case_data$dogs,
@@ -13,10 +21,14 @@ plot(case_data$cases~case_data$dogs,
 # Cases against urban status
 plot(case_data$cases~case_data$urban, 
      ylab="Cases",xlab="Urban",cex.lab=1.2,
-     bty="l",pch=19,col=c("blue","orange"))
+     pch=19,col=c("blue","orange"))
 
 # Cases against coastal status
 plot(case_data$cases~case_data$coastal, 
      ylab="Cases",xlab="Coastal",cex.lab=1.2,
-     bty="l",pch=19,col=c("blue","orange"))
+     pch=19,col=c("blue","orange"))
+
+# Close pdf
+dev.off()
+
 
